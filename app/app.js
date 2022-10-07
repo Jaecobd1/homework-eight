c08613256fdb9d71137e9f3af10c4175e1a11ba7
Jaecobd1 <jaecob.d1@gmail.com>
2022-10-07 09:21:06 -0400

    adding all files

--- app/app.js
+++ app/app.js
@@ -1,19 +1,11 @@
 $(document).ready(function() {
     initListeners();
-    $("#app").html(homeContent);
+    console.log(MODEL.myName)
+    MODEL.updateView("homeContent");
 });
 
 
-var homeContent = `<h1>HOME</h1>  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum animi deserunt quod. Ea dolorem assumenda officia voluptas quidem laboriosam aut deserunt quo, fuga nisi cupiditate.</p>
-        <p>Magni rerum, aliquid beatae, accusantium voluptates odit iure consequatur debitis, nostrum maxime nam veniam distinctio in? A quibusdam quis laborum. Minima pariatur dolorum natus recusandae.</p>
-        <p>Veritatis eum laboriosam non, dolore aperiam ad libero praesentium. Accusamus cum perspiciatis, consequuntur, fugit ullam ducimus eveniet, perferendis corporis recusandae iste quod ut quia similique.</p>`;
-var aboutContent = `<h1>About</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam qui esse, facilis dolorem sequi deserunt. Suscipit dolorum, in necessitatibus minima at illo blanditiis perferendis veniam quasi obcaecati. Nisi!</p>
-        <p>Doloribus magni qui ratione, veniam beatae perferendis possimus perspiciatis aperiam ipsam unde a voluptate quis reprehenderit illum. Excepturi pariatur nostrum sed inventore? Mollitia deleniti tempora ipsam architecto temporibus.</p>
-        <p>Iure animi quaerat, molestias voluptate cumque corporis at, fugit, assumenda voluptates enim inventore libero dolor laborum illo velit perspiciatis saepe? Saepe maxime a temporibus laudantium dolores delectus alias.</p>`;
-var productsContent = `<h1>Products</h1>  <p><span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim eum sed iste odio nobis fugiat beatae.</span><span>Hic tempora corrupti totam libero cupiditate vero cumque labore, est assumenda molestiae, animi nulla possimus aliquid.</span><span>Optio, ad? Quo temporibus, quasi, et, voluptatibus obcaecati optio consequatur nam quibusdam eligendi vero illum corrupti.</span></p>`;
-var contactContent = `<h1>Contact</h1> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo soluta, ea animi modi, dolor optio pariatur assumenda quasi ducimus dolorum culpa illo enim iste!</p>
-        <p>Accusamus dolorem aspernatur explicabo molestias architecto nisi id exercitationem reprehenderit maiores nihil eius eligendi sequi, tenetur ab pariatur molestiae! Ullam eveniet laborum dolorem enim.</p>
-        <p>Quod laboriosam dolor, sapiente quia atque aperiam repudiandae ex, in ullam, aliquam minima tempora suscipit natus sit sint culpa voluptates excepturi nihil totam voluptate.</p>`;
+
 
 
 
@@ -26,9 +18,9 @@ function initListeners() {
         console.log("a id " + btn);
 
         let contentID = btn + "Content"
-        console.log(contentID);
 
-        $("#app").html(eval(contentID));
+        MODEL.updateView(contentID)
+
 
         if (btn == "cbc") {
             $("nav").css("background-color", "red");
